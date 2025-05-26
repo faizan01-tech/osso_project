1ecacef6e8169e657336eceaf1b6036df6969e78
faizan01-tech <faizkh2014@gmail.com>
2025-05-27 04:42:06 +0530

    Updated project structure and removed unnecessary platform files

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:osso_project/screens/welcome_screen.dart';
 import 'package:osso_project/theme/theme.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
+
+  WidgetsFlutterBinding.ensureInitialized();
+ await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
